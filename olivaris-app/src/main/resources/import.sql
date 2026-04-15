6c69caf5cf4b80186f61ede07280ba3046140ef5
Jorge <canomelero1@gmail.com>
2026-04-15 13:30:59 +0200

    removed insert values on entity permission from import.sql

--- olivaris-app/src/main/resources/import.sql
+++ olivaris-app/src/main/resources/import.sql
@@ -3,8 +3,3 @@ INSERT INTO role(name) VALUES ('ROLE_BASIC');
 
 INSERT INTO entity_role(name) VALUES ('ROLE_ADMIN');
 INSERT INTO entity_role(name) VALUES ('ROLE_FARMER');
-
-INSERT INTO entity_permission(name) VALUES ('READ_REA');
-INSERT INTO entity_permission(name) VALUES ('READ_CUE');
-INSERT INTO entity_permission(name) VALUES ('WRITE_REA');
-INSERT INTO entity_permission(name) VALUES ('WRITE_CUE');
--- olivaris-app/src/test/resources/import.sql
+++ olivaris-app/src/test/resources/import.sql
@@ -1,12 +1,7 @@
 -- Initial test data for H2 database
 INSERT INTO role(name) VALUES ('ROLE_ADMIN');
-INSERT INTO role(name) VALUES ('ROLE_ENTITY_ADMIN');
+INSERT INTO role(name) VALUES ('ROLE_BASIC');
 INSERT INTO role(name) VALUES ('ROLE_FARMER');
 
 INSERT INTO entity_role(name) VALUES ('ROLE_ADMIN');
 INSERT INTO entity_role(name) VALUES ('ROLE_FARMER');
-
-INSERT INTO entity_permission(name) VALUES ('READ_REA');
-INSERT INTO entity_permission(name) VALUES ('READ_CUE');
-INSERT INTO entity_permission(name) VALUES ('WRITE_REA');
-INSERT INTO entity_permission(name) VALUES ('WRITE_CUE');
